7a720a59f13ed7656aa7c3f73f653fcc4f9d3c3f
qjasn <1766684296@qq.com>
2024-01-13 17:48:16 +0800

    Update

--- src/latexify/__init__.py
+++ src/latexify/__init__.py
@@ -12,7 +12,7 @@ from latexify import frontend, generate_latex
 Style = generate_latex.Style
 
 get_latex = generate_latex.get_latex
-
+get_latex_with_code = generate_latex.get_latex_with_code
 algorithmic = frontend.algorithmic
 expression = frontend.expression
 function = frontend.function
